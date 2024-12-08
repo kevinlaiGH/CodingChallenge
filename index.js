7b983b4d4026f02c1d13a8d8d9eb48317e7db27c
kevinlaiGH <kevin.km.lai@gmail.com>
2024-12-08 13:48:50 +1100

    add sortAndSaveData function to read, sort, and save JSON data

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const sortObjectAttributes = (obj) => {
   const sortedObj = {};
   Object.keys(obj)
@@ -14,7 +17,24 @@ const parseJsonData = (data) => {
   return JSON.stringify(jsonData, null, 2);
 };
 
+const sanitizedPath = (filePath) => path.normalize(filePath);
+
+const sortAndSaveData = async (inputFile, outputFile) => {
+  try {
+    const data = await fs.promises.readFile(sanitizedPath(inputFile), 'utf8');
+    const sortedData = parseJsonData(data);
+    await fs.promises.writeFile(sanitizedPath(outputFile), sortedData);
+    console.info(`Data attributes sorted and saved to ${outputFile}`);
+  } catch (err) {
+    console.error('Error processing file:', err);
+    throw err;
+  }
+};
+
+sortAndSaveData('data.json', 'sorted_data.json');
+
 module.exports = {
   sortObjectAttributes,
   parseJsonData,
+  sortAndSaveData,
 };
